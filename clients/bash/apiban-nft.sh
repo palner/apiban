411ffef947ff2560c7667d340642735e3c55d401
Fred Posner <fred@qxork.com>
2020-09-01 10:48:17 -0400

    * apiban-nft.sh: add license * abiban.sh: add license

--- clients/bash/apiban-nft.sh
+++ clients/bash/apiban-nft.sh
@@ -1,4 +1,19 @@
 #! /bin/bash
+# * This file is part of APIBAN.org.
+# *
+# * apiban-iptables-client is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version
+# *
+# * apiban-iptables-client is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 NOW=$(date +"%Y-%m-%d %H:%M:%S")
 
 # APIKEY and last known ID are stored in config file
